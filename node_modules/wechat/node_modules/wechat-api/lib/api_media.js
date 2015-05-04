90b2b97e90b3706c9bafefb4f3b1fbeb49f9f35e
Kelvin <4DKelvin@gmail.com>
2015-05-04 13:35:10 +0800

    用别人的

---
+++ node_modules/wechat/node_modules/wechat-api/lib/api_media.js
@@ -0,0 +1,121 @@
+var path = require('path');
+var fs = require('fs');
+
+var formstream = require('formstream');
+var util = require('./util');
+var wrapper = util.wrapper;
+
+/**
+ * 新增临时素材，分别有图片（image）、语音（voice）、视频（video）和缩略图（thumb）
+ * 详情请见：<http://mp.weixin.qq.com/wiki/5/963fc70b80dc75483a271298a76a8d59.html>
+ * Examples:
+ * ```
+ * api.uploadMedia('filepath', type, callback);
+ * ```
+ * Callback:
+ *
+ * - `err`, 调用失败时得到的异常
+ * - `result`, 调用正常时得到的对象
+ *
+ * Result:
+ * ```
+ * {"type":"TYPE","media_id":"MEDIA_ID","created_at":123456789}
+ * ```
+ * Shortcut:
+ *
+ * - `exports.uploadImage(filepath, callback);`
+ * - `exports.uploadVoice(filepath, callback);`
+ * - `exports.uploadVideo(filepath, callback);`
+ * - `exports.uploadThumb(filepath, callback);`
+ *
+ * @param {String} filepath 文件路径
+ * @param {String} type 媒体类型，可用值有image、voice、video、thumb
+ * @param {Function} callback 回调函数
+ */
+exports.uploadMedia = function (filepath, type, callback) {
+  this.preRequest(this._uploadMedia, arguments);
+};
+
+/*!
+ * 上传多媒体文件的未封装版本
+ */
+exports._uploadMedia = function (filepath, type, callback) {
+  var that = this;
+  fs.stat(filepath, function (err, stat) {
+    if (err) {
+      return callback(err);
+    }
+    var form = formstream();
+    form.file('media', filepath, path.basename(filepath), stat.size);
+    var url = that.prefix + 'media/upload?access_token=' + that.token.accessToken + '&type=' + type;
+    var opts = {
+      dataType: 'json',
+      type: 'POST',
+      timeout: 60000, // 60秒超时
+      headers: form.headers(),
+      stream: form
+    };
+    that.request(url, opts, wrapper(callback));
+  });
+};
+
+['image', 'voice', 'video', 'thumb'].forEach(function (type) {
+  var method = 'upload' + type[0].toUpperCase() + type.substring(1);
+  exports[method] = function (filepath, callback) {
+    this.uploadMedia(filepath, type, callback);
+  };
+});
+
+/**
+ * 获取临时素材
+ * 详情请见：<http://mp.weixin.qq.com/wiki/11/07b6b76a6b6e8848e855a435d5e34a5f.html>
+ * Examples:
+ * ```
+ * api.getMedia('media_id', callback);
+ * ```
+ * Callback:
+ *
+ * - `err`, 调用失败时得到的异常
+ * - `result`, 调用正常时得到的文件Buffer对象
+ * - `res`, HTTP响应对象
+ *
+ * @param {String} mediaId 媒体文件的ID
+ * @param {Function} callback 回调函数
+ */
+exports.getMedia = function (mediaId, callback) {
+  this.preRequest(this._getMedia, arguments);
+};
+
+/*!
+ * 获取临时素材的未封装版本
+ */
+exports._getMedia = function (mediaId, callback) {
+  var url = this.prefix + 'media/get?access_token=' + this.token.accessToken + '&media_id=' + mediaId;
+  var opts = {
+    timeout: 60000 // 60秒超时
+  };
+  this.request(url, opts, wrapper(function (err, data, res) {
+    // handle some err
+    if (err) {
+      return callback(err);
+    }
+    var contentType = res.headers['content-type'];
+    if (contentType === 'application/json') {
+      var ret;
+      try {
+        ret = JSON.parse(data);
+        if (ret.errcode) {
+          err = new Error(ret.errmsg);
+          err.name = 'WeChatAPIError';
+        }
+      } catch (ex) {
+        callback(ex, data, res);
+        return;
+      }
+      callback(err, ret, res);
+    } else {
+      // 输出Buffer对象
+      callback(null, data, res);
+    }
+  }));
+};
--- server.js
+++ server.js
@@ -1,3 +1,45 @@
 var express = require("express"),
-  app = express();
-app.use(express.static('resources')).listen(4000);
+  app = express(), wechat = require('wechat'), config = {
+    token: 'token',
+    appid: 'appid',
+    encodingAESKey: 'encodinAESKey'
+  };
+
+app.use(express.query()); // Or app.use(express.query());
+app.use('/wechat', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3",
+        thumbMediaId: "thisThumbMediaId"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+}));
+app.listen(4000);
